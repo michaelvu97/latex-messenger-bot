66e752686e7d03818fe2bf8e3e62d30d461edc29
michaelvu97 <michael.vu.97@outlook.com>
2017-03-13 16:25:30 -0400

    more typos

--- app.js
+++ app.js
@@ -414,7 +414,7 @@ function sendImageMessage(recipientId) {
       attachment: {
         type: "image",
         payload: {
-          url: SERVER_URL + '/output/' + recipientID + '.png'
+          url: SERVER_URL + '/output/' + recipientId + '.png'
         }
       }
     }
